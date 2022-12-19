7199552869c00bf5180833d88d660692370a4af7
Eric Vento <vento.eric@gmail.com>
2022-12-19 10:46:33 -0600

    changed directory layout

---
+++ day1/target/debug/aoc2022.d
@@ -0,0 +1,1 @@
+C:\Users\vento\OneDrive\programming\aoc2022\target\debug\aoc2022.exe: C:\Users\vento\OneDrive\programming\aoc2022\src\main.rs
---
+++ day1/target/debug/deps/aoc2022-40830f4c5d1e9bba.d
@@ -0,0 +1,7 @@
+C:\Users\vento\OneDrive\programming\aoc2022\target\debug\deps\aoc2022-40830f4c5d1e9bba.rmeta: src\main.rs
+
+C:\Users\vento\OneDrive\programming\aoc2022\target\debug\deps\aoc2022-40830f4c5d1e9bba.d: src\main.rs
+
+src\main.rs:
+
+# env-dep:CLIPPY_ARGS=
