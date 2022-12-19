fe063e0e33b056e5df91e225a0cf6b9a622335e2
Eric Vento <vento.eric@gmail.com>
2022-12-18 22:00:06 -0600

    day 1, part 1 completed

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,20 @@ fn day1() {
     let day1filetext:String = fs::read_to_string(r"data\day1.txt")
         .expect("Invalid File.");
 
+    let mut cal_sum: i32=0; 
+    let mut cal_the_big_one=0; 
     for cal in day1filetext.lines(){
         if cal.is_empty() {
-            println!("END"); 
+            if cal_sum>cal_the_big_one {
+                cal_the_big_one=cal_sum; 
+            }
+            cal_sum = 0; 
         }
         else {
-            let my_int: i32 = cal.parse().unwrap();
-            println!("{}",my_int)
+            let cal_int: i32 = cal.parse().unwrap();
+            cal_sum += cal_int; 
         }
     }
+    println!("{}",cal_the_big_one);
 }
 
