0a449eb743f4a2ec81c0522440235f875978262e
Eric Vento <vento.eric@gmail.com>
2022-12-18 20:52:24 -0600

    paul introduced me to cargo clippy

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,14 @@ fn day1() {
     let day1filetext:String = fs::read_to_string(r"data\day1.txt")
         .expect("Invalid File.");
 
-    println!("{}",day1filetext);
+    for cal in day1filetext.lines(){
+        if cal.is_empty() {
+            println!("END"); 
+        }
+        else {
+            let my_int: i32 = cal.parse().unwrap();
+            println!("{}",my_int)
+        }
+    }
 }
+
