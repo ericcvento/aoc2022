cd62a50228b0f1c9f399ad81a105043ca9fc33c5
Eric Vento <vento.eric@gmail.com>
2022-12-18 22:18:00 -0600

    ran cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,24 @@
-
-use std::{fs};
+use std::fs;
 
 fn main() {
-    day1(); 
+    day1();
 }
 
 fn day1() {
-    let day1filetext:String = fs::read_to_string(r"data\day1.txt")
-        .expect("Invalid File.");
+    let day1filetext: String = fs::read_to_string(r"data\day1.txt").expect("Invalid File.");
 
-    let mut cal_sum: i32=0; 
-    let mut cal_the_big_one=0; 
-    for cal in day1filetext.lines(){
+    let mut cal_sum: i32 = 0;
+    let mut cal_the_big_one = 0;
+    for cal in day1filetext.lines() {
         if cal.is_empty() {
-            if cal_sum>cal_the_big_one {
-                cal_the_big_one=cal_sum; 
+            if cal_sum > cal_the_big_one {
+                cal_the_big_one = cal_sum;
             }
-            cal_sum = 0; 
-        }
-        else {
+            cal_sum = 0;
+        } else {
             let cal_int: i32 = cal.parse().unwrap();
-            cal_sum += cal_int; 
+            cal_sum += cal_int;
         }
     }
-    println!("this is the big one: {}",cal_the_big_one);
+    println!("this is the big one: {}", cal_the_big_one);
 }
-
