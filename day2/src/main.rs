fd676a1dc5620b149c4ec4b7e3410c332accd830
Eric Vento <vento.eric@gmail.com>
2022-12-20 23:25:30 -0600

    added code to read puzzle input

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,11 @@
+use std::fs;
 fn main() {
     println!("Hello, world!");
+
+    let filetext: String = fs::read_to_string(r"data\day2input.txt")
+        .expect("Invalid File.");
+
+    for l in filetext.lines() {
+        println!("{}",l); 
+    }
 }
