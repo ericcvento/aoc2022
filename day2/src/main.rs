ad303ca551757b512c1d869f9f5c7c4922e9cf5b
Eric Vento <vento.eric@gmail.com>
2022-12-22 10:12:06 -0600

    completed day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,7 +2,8 @@ use std::fs;
 use substring::Substring;
 
 fn main() {
-    part1(); 
+    part1();
+    part2();
 }
 
 fn part1() {
@@ -59,45 +60,64 @@ fn part2() {
     for line in filetext.lines() {
         let myplaychar: &str = line.substring(2, 3);
         let yourplaychar: &str = line.substring(0, 1);
-        //X for Rock, Y for Paper, and Z for Scissors
-        //shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
-        let mut myplay: &str = "";
-        let mut myplaypoints: i32 = 0;
-        if myplaychar == "X" {
-            myplay = "Rock";
-            myplaypoints = 1;
-        } else if myplaychar == "Y" {
-            myplay = "Paper";
-            myplaypoints = 2;
-        } else if myplaychar == "Z" {
-            myplay = "Scissors";
-            myplaypoints = 3;
-        }
         //A for Rock, B for Paper, and C for Scissors
         let mut yourplay: &str = "";
-        let mut yourplaypoints: i32 = 0;
         if yourplaychar == "A" {
             yourplay = "Rock";
-            yourplaypoints = 1;
         } else if yourplaychar == "B" {
             yourplay = "Paper";
-            yourplaypoints = 2;
         } else if yourplaychar == "C" {
             yourplay = "Scissors";
-            yourplaypoints = 3;
         }
-
-        let mut outcome: i32 = 0;
-        if ((myplay == "Rock") & (yourplay == "Scissors"))
-            | ((myplay == "Paper") & (yourplay == "Rock"))
-            | ((myplay == "Scissors") & (yourplay == "Paper"))
-        {
-            outcome = 6;
-        } else if myplay == yourplay {
+        //X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
+        let mut outcome = 0;
+        if myplaychar == "X" {
+            outcome = 0;
+        } else if myplaychar == "Y" {
             outcome = 3;
+        } else if myplaychar == "Z" {
+            outcome = 6;
         }
-        score += myplaypoints + outcome;
+        let mut myplay: &str = "";
+        let mut myplaypoints = 0;
+        //1 for Rock, 2 for Paper, and 3 for Scissors
+        if yourplay == "Rock" {
+            if outcome == 0 {
+                myplay = "Scissors";
+                myplaypoints = 3;
+            } else if outcome == 3 {
+                myplay = "Rock";
+                myplaypoints = 1;
+            } else if outcome == 6 {
+                myplay = "Paper";
+                myplaypoints = 2;
+            }
+        }
+        if yourplay == "Paper" {
+            if outcome == 0 {
+                myplay = "Rock";
+                myplaypoints = 1;
+            } else if outcome == 3 {
+                myplay = "Paper";
+                myplaypoints = 2;
+            } else if outcome == 6 {
+                myplay = "Scissors";
+                myplaypoints = 3;
+            }
+        }
+        if yourplay == "Scissors" {
+            if outcome == 0 {
+                myplay = "Paper";
+                myplaypoints = 2;
+            } else if outcome == 3 {
+                myplay = "Scissors";
+                myplaypoints = 3;
+            } else if outcome == 6 {
+                myplay = "Rock";
+                myplaypoints = 1;
+            }
+        }
+        score += outcome + myplaypoints;
     }
     println!("My Score: {}", score);
 }
-
