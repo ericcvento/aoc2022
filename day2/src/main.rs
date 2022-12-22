78be6806a9c2df638976579385806d3e56ac9e36
Eric Vento <vento.eric@gmail.com>
2022-12-21 23:24:18 -0600

    process input line by line and split string into two variables

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,11 +1,15 @@
-use std::fs;
+use std::{fs};
+use substring::Substring;
+
 fn main() {
     println!("Hello, world!");
 
     let filetext: String = fs::read_to_string(r"data\day2input.txt")
         .expect("Invalid File.");
 
-    for l in filetext.lines() {
-        println!("{}",l); 
+    for line in filetext.lines() {
+
+        let myplay:&str=(line.substring(0,1)); 
+        let yourplay:&str = line.substring(2,3); 
     }
 }
