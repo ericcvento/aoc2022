d3b100533b289ba2c1938fb17c06757fc613ef3e
Eric Vento <vento.eric@gmail.com>
2022-12-21 23:31:45 -0600

    process input line by line and split string into two variables

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -9,7 +9,9 @@ fn main() {
 
     for line in filetext.lines() {
 
-        let myplay:&str=(line.substring(0,1)); 
-        let yourplay:&str = line.substring(2,3); 
+        let myplaychar:&str=(line.substring(0,1)); 
+        let yourplaychar:&str = line.substring(2,3); 
+        //A for Rock, B for Paper, and C for Scissors
+        if myplaychar = ""
     }
 }
