cb6c3402c65f2388c9afebaba61d9b51e509bbe0
Eric Vento <vento.eric@gmail.com>
2022-12-24 13:44:58 -0600

    compared items from compartment 1 and compartment2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -8,9 +8,9 @@ fn main() {
 fn part1() {
     let filetext: String = fs::read_to_string(r"data\day3input.txt").expect("Invalid File.");
 
-    let mut compart1: &str = "";
-    let mut compart2: &str = "";
-    let mut compartsize: usize = 0;
+    let mut compart1: &str;
+    let mut compart2: &str;
+    let mut compartsize: usize;
 
     for line in filetext.lines() {
         compartsize = line.len() / 2;
@@ -19,5 +19,13 @@ fn part1() {
 
         println!("Ruck: {}, Compartment Size: {}", line, compartsize);
         println!("Compartment1:{}, Compartment2:{}", compart1, compart2);
+
+        for c1 in compart1.chars() {
+            for c2 in compart2.chars() {
+                if c1 == c2 {
+                    println!("{}", c1);
+                }
+            }
+        }
     }
 }
