d98a76017d20d64304662464fd73ff1fd78ed034
Eric Vento <vento.eric@gmail.com>
2022-12-24 10:56:24 -0600

    split rucksacks into two compartments

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use substring::Substring;
 
 fn main() {
     part1();
@@ -6,4 +7,17 @@ fn main() {
 
 fn part1() {
     let filetext: String = fs::read_to_string(r"data\day3input.txt").expect("Invalid File.");
+
+    let mut compart1: &str = "";
+    let mut compart2: &str = "";
+    let mut compartsize: usize = 0;
+
+    for line in filetext.lines() {
+        compartsize = line.len() / 2;
+        compart1 = line.substring(0, compartsize);
+        compart2 = line.substring(compartsize, line.len());
+
+        println!("Ruck: {}, Compartment Size: {}", line, compartsize);
+        println!("Compartment1:{}, Compartment2:{}", compart1, compart2);
+    }
 }
