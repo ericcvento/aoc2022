8ddc4b6e3e0c153443008032343e32dc4246c503
Eric Vento <vento.eric@gmail.com>
2022-12-26 16:24:23 -0600

    split elf pairs

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,23 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    part1(); 
+}
+
+fn part1() {
+    let filetext: String = fs::read_to_string(r"data\day4input.txt").expect("Invalid File.");
+    for l in filetext.lines() {
+        let mut elf1 : &str = "";
+        let mut elf2 : &str = "";
+        let mut i = 1; 
+        for pair in l.split(",") {
+            if i == 1 {
+                elf1=pair; 
+            } else if i == 2 {
+                elf2=pair; 
+            }
+            i+=1 ; 
+
+        }
+    }
 }
