6e16f1f6d3384a8237929545e7f9a297da6ca956
Eric Vento <vento.eric@gmail.com>
2022-12-26 20:12:26 -0600

    starting to figure out split_once

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -6,18 +6,8 @@ fn main() {
 
 fn part1() {
     let filetext: String = fs::read_to_string(r"data\day4input.txt").expect("Invalid File.");
-    for l in filetext.lines() {
-        let mut elf1 : &str = "";
-        let mut elf2 : &str = "";
-        let mut i = 1; 
-        for pair in l.split(",") {
-            if i == 1 {
-                elf1=pair; 
-            } else if i == 2 {
-                elf2=pair; 
-            }
-            i+=1 ; 
-
-        }
+    for elfpair in filetext.lines() {
+   
+        let (fst,snd) = elfpair.split_once(',').unwrap();
     }
 }
