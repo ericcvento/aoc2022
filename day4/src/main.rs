0b9e331e56b1936765de93ba66846986d60b8bc8
Eric Vento <vento.eric@gmail.com>
2022-12-27 10:29:44 -0600

    solved day 4, part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -2,6 +2,7 @@ use std::fs;
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -23,6 +24,38 @@ fn part1() {
             subsumed = 1;
         }
         if (elf2start <= elf1start) & (elf2end >= elf1end) {
+            subsumed = 1;
+        }
+        score += subsumed;
+    }
+    println!("{}", score);
+}
+
+fn part2() {
+    let mut score: i32 = 0;
+    let filetext: String = fs::read_to_string(r"data\day4input.txt").expect("Invalid File.");
+    for elfpair in filetext.lines() {
+        let (elf1, elf2) = elfpair.split_once(',').unwrap();
+        let (elf1start, elf1end) = elf1.split_once('-').unwrap();
+        let (elf2start, elf2end) = elf2.split_once('-').unwrap();
+
+        //is this OK to do?
+        let elf1start: i32 = elf1start.parse().unwrap();
+        let elf1end: i32 = elf1end.parse().unwrap();
+        let elf2start: i32 = elf2start.parse().unwrap();
+        let elf2end: i32 = elf2end.parse().unwrap();
+
+        let mut subsumed: i32 = 0;
+        if (elf1start <= elf2start) & (elf1end >= elf2start) {
+            subsumed = 1;
+        }
+        if (elf1start <= elf2end) & (elf1end >= elf2end) {
+            subsumed = 1;
+        }
+        if (elf2start <= elf1start) & (elf2end >= elf1start) {
+            subsumed = 1;
+        }
+        if (elf2start <= elf1end) & (elf2end >= elf1end) {
             subsumed = 1;
         }
         score += subsumed;
