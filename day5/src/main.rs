c2f2765a237a092087577030d6c8542f0c6cc773
Eric Vento <vento.eric@gmail.com>
2022-12-31 14:12:12 -0500

    solved day 5, part 1

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 fn part1() {
     let filetext: String = fs::read_to_string(r"data\day5input.txt").expect("Invalid File.");
 
-    //load crates onto their starting positions 
+    //load crates onto their starting positions
     let mut columns = vec![vec![]; 9];
     for l in filetext.lines() {
         if l.substring(1, 2) == "1" {
@@ -16,26 +16,30 @@ fn part1() {
         }
         let mut j = 1;
         for column in columns.iter_mut() {
-            column.push(l.substring(j, j + 1));
+            let crate_content = l.substring(j, j + 1);
+            if crate_content != " " {
+                column.push(crate_content);
+            }
             j += 4;
         }
     }
 
     //reverse order of crates on columns
     for i in columns.iter_mut() {
-        println!("{:?}",i); 
+        println!("{:?}", i);
         i.reverse();
-        println!("{:?}",i);
+        i.truncate(i.len());
+        println!("{:?}", i);
     }
 
     //crane directions
     for l in filetext.lines() {
         if l.substring(0, 4) == "move" {
-            let mut n : i32=0; 
-            let mut from :i32=0; 
-            let mut to: i32=0;             
-            for (i,ll)  in l.split_whitespace().enumerate() {
-                if i==1 {
+            let mut n: i32 = 0;
+            let mut from: usize = 0;
+            let mut to: usize = 0;
+            for (i, ll) in l.split_whitespace().enumerate() {
+                if i == 1 {
                     n = ll.parse().unwrap();
                 } else if i == 3 {
                     from = ll.parse().unwrap();
@@ -43,8 +47,18 @@ fn part1() {
                     to = ll.parse().unwrap();
                 }
             }
-        //execute instructions; 
-        
+            //execute instructions;
+            //println!("move {} crates from column {} to {}.",n,from,to);
+            for i in 0..n {
+                let crate_contents: &str = columns[from - 1].pop().unwrap();
+                columns[to - 1].push(crate_contents);
+                columns[from - 1].shrink_to_fit();
+                columns[to - 1].shrink_to_fit();
+            }
         }
+    }
+    println!("Columns");
+    for i in columns.iter_mut() {
+        println!("{:?}", i);
     }
 }
