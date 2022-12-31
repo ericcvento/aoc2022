c93349579a3ed2ab734008276b9d96db0b2f7b2b
Eric Christopher Vento <vento.eric@gmail.com>
2022-12-31 13:21:39 -0500

    parsed instructions in to 3 variables n, from, and to.

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -8,24 +8,43 @@ fn main() {
 fn part1() {
     let filetext: String = fs::read_to_string(r"data\day5input.txt").expect("Invalid File.");
 
+    //load crates onto their starting positions 
     let mut columns = vec![vec![]; 9];
     for l in filetext.lines() {
         if l.substring(1, 2) == "1" {
             break;
         }
         let mut j = 1;
-        for i in 0..8 {
-            columns[i].push(l.substring(j, j + 1));
+        for column in columns.iter_mut() {
+            column.push(l.substring(j, j + 1));
             j += 4;
         }
     }
 
     //reverse order of crates on columns
-    println!("{:?}",columns[0]); 
-    for i in 0..8 {
-        println!("Stack: {}",i); 
-        println!("{:?}",columns[i]); 
-        columns[i].reverse();
-        println!("{:?}",columns[i]);
+    for i in columns.iter_mut() {
+        println!("{:?}",i); 
+        i.reverse();
+        println!("{:?}",i);
+    }
+
+    //crane directions
+    for l in filetext.lines() {
+        if l.substring(0, 4) == "move" {
+            let mut n : i32=0; 
+            let mut from :i32=0; 
+            let mut to: i32=0;             
+            for (i,ll)  in l.split_whitespace().enumerate() {
+                if i==1 {
+                    n = ll.parse().unwrap();
+                } else if i == 3 {
+                    from = ll.parse().unwrap();
+                } else if i == 5 {
+                    to = ll.parse().unwrap();
+                }
+            }
+        //execute instructions; 
+        
+        }
     }
 }
