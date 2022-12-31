0712e5aad781bec4f2c010995972f8abd9c12ae9
Eric Vento <vento.eric@gmail.com>
2022-12-31 00:25:26 -0500

    day 5-loaded the cargo crates into vecs absracting the stacks

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,26 +1,26 @@
 use std::fs;
-use substring::Substring; 
+use substring::Substring;
 
 fn main() {
-    part1(); 
+    part1();
 }
 
-fn part1(){
+fn part1() {
     let filetext: String = fs::read_to_string(r"data\day5input.txt").expect("Invalid File.");
-    for l in filetext.lines() {
 
-        let c1 = l.substring(1,2); 
-        let c2 = l.substring(5,6); 
-        let c3 = l.substring(9,10); 
-        let c4 = l.substring(13,14); 
-        let c5 = l.substring(17,18); 
-        let c6 = l.substring(21,21); 
-        let c7 = l.substring(25,26); 
-        let c8 = l.substring(29,30); 
-        let c9 = l.substring(33,34); 
-        
-        if c1 == "1" {
-            break; 
+    let mut columns = vec![vec![]; 9];
+    for l in filetext.lines() {
+        if l.substring(1, 2) == "1" {
+            break;
         }
+        let mut j = 1;
+        for i in 0..8 {
+            columns[i].push(l.substring(j, j + 1));
+            j += 4;
+        }
+    }
+
+    for i in 0..8 {
+        println!("{:?}", columns[i]);
     }
 }
