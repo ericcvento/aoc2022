710a2468fc64d7a8ef76bbb5325662066aa587e9
Eric Vento <vento.eric@gmail.com>
2022-12-31 23:27:50 -0500

    solved day 6, part 1

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,12 +1,25 @@
-use std::fs; 
+use std::fs;
 
 fn main() {
-    read_data();
+    part1();
 }
 
-fn read_data() {
+fn part1() {
     let filetext: String = fs::read_to_string(r"data\day6input.txt").expect("Invalid File.");
-    for s in filetext.chars() {
-        println!("{}",s); 
+    let mut four = Vec::new();
+    for (i, s) in filetext.chars().enumerate() {
+        four.push(s);
+        if four.len() >= 4 {
+            let mut four_copy = four.to_vec();
+            four_copy.sort();
+            four_copy.dedup();
+            if four_copy.len() == 4 {
+                println!("Part 1 Solution: {}", i + 1);
+                break;
+            }
+            four.reverse();
+            four.pop();
+            four.reverse();
+        }
     }
 }
