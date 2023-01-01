65b4ebe71ce35422df3b8a7e6dbd164cf3936567
Eric Christopher Vento <vento.eric@gmail.com>
2023-01-01 12:14:55 -0500

    using ownership and &/&str incorrectly, but have captured each of the possible CD suffixes

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,13 +1,42 @@
-use std::fs; 
+use std::fs;
+use substring::Substring;
 
 fn read_data() -> String {
-    let filetext:String = fs::read_to_string(r"data\day7input.txt").expect("Invalid File.");
-    return filetext; 
+    let filetext: String = fs::read_to_string(r"data\day7input.txt").expect("Invalid File.");
+    return filetext;
+}
+
+fn map_directories((history): (String)) {
+    let mut directory: Vec<&str> = Vec::new();
+    let mut cwd: &str = "/";
+
+    for h in history.lines() {
+        //these are commands
+        if h.substring(0, 1) == "$" {
+            let command: &str = &h[2..4].trim();
+            let command_suffix: &str = h[4..].trim();
+
+            //checks & tests
+            if command == "ls" {
+                assert!(command_suffix.is_empty());
+            }
+
+            //navigate
+            if command == "cd" {
+                if command_suffix == ".." {
+                    println!("{} {}-change to parent dir", command, command_suffix);
+                } else if command_suffix == "/" {
+                    println!("{} {}-back to root", command, command_suffix);
+                } else {
+                    assert!(!command_suffix.is_empty()); 
+                    println!("{} {}-move down to {}",command, command_suffix,command_suffix); 
+                }
+            }
+        }
+    }
 }
 
 fn main() {
-    let filetext=read_data();
-    for l in filetext.lines() {
-        println!("{}",l); 
-    }
+    let filetext = read_data();
+    map_directories(filetext);
 }
