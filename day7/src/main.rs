277f07abf49c9dd9e16d6a09a10ecfb064422637
Eric Vento <vento.eric@gmail.com>
2023-01-01 08:04:51 -0500

    wrote function to read data for day 7

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,13 @@
+use std::fs; 
+
+fn read_data() -> String {
+    let filetext:String = fs::read_to_string(r"data\day7input.txt").expect("Invalid File.");
+    return filetext; 
+}
+
 fn main() {
-    println!("Hello, world!");
+    let filetext=read_data();
+    for l in filetext.lines() {
+        println!("{}",l); 
+    }
 }
