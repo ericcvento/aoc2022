b3d251023fed47600d3eec25dd6f18736cfcf55e
Eric Christopher Vento <vento.eric@gmail.com>
2023-01-02 10:21:32 -0500

    fmt

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -8,14 +8,13 @@ fn read_data() -> String {
 
 fn main() {
     let filetext = read_data();
-    //go through each line of history 
+    //go through each line of history
     for line in filetext.lines() {
-        //these are commands; 
-        if line.substring(0,1)=="$" {
-        
-        //these are output
+        //these are commands;
+        if line.substring(0, 1) == "$" {
+
+            //these are output
         } else {
-            
         }
     }
 }
