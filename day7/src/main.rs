6c4251ce8ca1743c4fb65fb98ab3701f80a51cfc
Eric Christopher Vento <vento.eric@gmail.com>
2023-01-02 10:44:42 -0500

    clippied and fmt

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -2,19 +2,25 @@ use std::fs;
 use substring::Substring;
 
 fn read_data() -> String {
-    let filetext: String = fs::read_to_string(r"data\day7input.txt").expect("Invalid File.");
-    return filetext;
+    let ft: String = fs::read_to_string(r"data\day7input.txt").expect("Invalid File.");
+    ft
 }
 
 fn main() {
     let filetext = read_data();
+
+    //initialize cwd as root
+    let mut current_working_directory = String::from("/");
     //go through each line of history
     for line in filetext.lines() {
-        //these are commands;
         if line.substring(0, 1) == "$" {
-
-            //these are output
+            //these are commands;
+            let mut command_split = line.split('$');
+            command_split.next();
+            let command: &str = command_split.next().unwrap();
+            println!("{}", command);
         } else {
+            //these are output
         }
     }
 }
