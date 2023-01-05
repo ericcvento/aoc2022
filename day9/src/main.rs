362115c4679b82e95c538b4ed354b5304bb50df6
Eric Vento <vento.eric@gmail.com>
2023-01-05 16:06:44 -0500

    fixed a typo in read_data

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,10 +1,13 @@
 use std::fs; 
 
 fn read_data() -> String {
-    let ft: String = fs::read_to_string(r"data\day8input.txt").expect("Invalid File.");
+    let ft: String = fs::read_to_string(r"data\day9input.txt").expect("Invalid File.");
     ft
 }
 
 fn main() {
     let input_text=read_data(); 
+    for l in input_text.lines() {
+        println!("{l}"); 
+    }
 }
