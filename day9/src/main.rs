7256a11cf7e0747a7b96252ed3f9d384052e1450
Eric Vento <vento.eric@gmail.com>
2023-01-06 09:56:20 -0500

    cargo fmt

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -7,26 +7,29 @@ fn read_data() -> String {
 
 fn push_rope(moves: String) -> i32 {
     let tail_spaces = 0;
-    let mut head_loc: [i32;2] = [0,0];
-    let mut tail_loc: [i32;2] = [0,0];
+    let mut head_loc: [i32; 2] = [0, 0];
+    let mut tail_loc: [i32; 2] = [0, 0];
 
-    for (i,l) in moves.lines().enumerate() {
+    for (i, l) in moves.lines().enumerate() {
         let instructions: Vec<&str> = l.split_whitespace().collect();
         let direction = instructions[0];
         let moves: i32 = instructions[1].parse().unwrap();
 
-        let mut position_change: [i32;2] = [0,0];
+        let mut position_change: [i32; 2] = [0, 0];
         for m in 0..moves {
             if direction == "U" {
-                position_change=[0,1]
+                position_change = [0, 1]
             } else if direction == "D" {
-                position_change=[0,-1]
+                position_change = [0, -1]
             } else if direction == "L" {
-                position_change=[-1,0]
+                position_change = [-1, 0]
             } else if direction == "R" {
-                position_change=[1,0]
+                position_change = [1, 0]
             }
-            head_loc=[head_loc[0]+position_change[0],head_loc[1]+position_change[1]]; 
+            head_loc = [
+                head_loc[0] + position_change[0],
+                head_loc[1] + position_change[1],
+            ];
         }
     }
     tail_spaces
