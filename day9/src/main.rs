008b5a09fe3469ef03743a13f6b380ae588c8a8c
Eric Vento <vento.eric@gmail.com>
2023-01-05 22:29:47 -0500

    started function on tracking location of head and tail of rope

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,13 +1,26 @@
-use std::fs; 
+use std::fs;
+
+type Coord = (i32, i32);
 
 fn read_data() -> String {
     let ft: String = fs::read_to_string(r"data\day9input.txt").expect("Invalid File.");
     ft
 }
 
-fn main() {
-    let input_text=read_data(); 
-    for l in input_text.lines() {
-        println!("{l}"); 
+fn push_rope(moves: String) -> i32 {
+    let t_spaces = 0;
+    let mut head_loc: Coord = (0, 0);
+    let mut tail_loc: Coord = (0, 0);
+
+    for l in moves.lines(){
+        println!("{}",l); 
+        
     }
+
+    t_spaces
+}
+
+fn main() {
+    let input_text = read_data();
+    push_rope(input_text.clone()); 
 }
