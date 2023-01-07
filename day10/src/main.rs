284b0e178c89bc51f7469e7a87b1a51c6ae7138d
Eric Vento <vento.eric@gmail.com>
2023-01-07 16:04:10 -0500

    solved day 10, part 1---code is kind of a mess though

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -18,32 +18,29 @@ fn process_instructions(output: String) -> HashMap<i32, (i32, String)> {
     let mut register: i32 = 1;
     let mut register_history = HashMap::new();
 
-    for (i, line) in output.lines().enumerate() {
+    for (_i, line) in output.lines().enumerate() {
         //parse instructions
+        let mut x = 0;
         if line.substring(0, 4) == "addx" {
-            for c in 0..2 {
+            for _c in 0..2 {
                 cycle += 1;
-                if c == 1 {
-                    register += line.substring(5, line.len()).parse::<i32>().unwrap();
-                }
                 register_history.insert(cycle, (register, line.to_string()));
             }
+            x = line.substring(5, line.len()).parse::<i32>().unwrap();
         } else {
             cycle += 1;
             register_history.insert(cycle, (register, line.to_string()));
         }
+        register += x;
     }
     register_history
 }
 
 fn main() {
     let input_text = read_data();
-
-    let input_text = read_test_data();
     let output_history = process_instructions(input_text);
 
     let mut part1_solution = 0;
-
     for i in 1..=output_history.len() {
         println!("{i}-{:?}", output_history[&(i as i32)]);
         if (i == 20) | ((i as i32 - 20) % 40 == 0) {
@@ -51,6 +48,5 @@ fn main() {
             part1_solution += (output_history[&(i as i32)].0) * (i as i32);
         }
     }
-
     println!("the solution to part 1 is {part1_solution}");
 }
