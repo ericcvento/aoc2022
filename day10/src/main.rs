c69d045f7dfbbe77d73596e181b1b76271148867
Eric Vento <vento.eric@gmail.com>
2023-01-07 12:01:33 -0500

    added a comment for later

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,20 +1,29 @@
 use std::fs;
+use substring::Substring;
 
 fn read_data() -> String {
     let ft: String = fs::read_to_string(r"data\day10input.txt").expect("Invalid File.");
     ft
 }
 
-fn process_instructions (output:String) -> i32 {
-
-    for (i,line) in output.lines().enumerate() {
-        println!("{line}"); 
+fn process_instructions(output: String) -> i32 {
+    //initialize register
+    let mut cycles: i32 = 0;
+    let mut register: i32 = 1;
+    let mut x: i32 = 0;
+    for (_i, line) in output.lines().enumerate() {
+        //parse instructions
+        if line.substring(0, 4) == "addx" {
+            //need to output register at certain cycles, add loop that takes 2 steps here; 
+            cycles += 2;
+            x += line.substring(5, line.len()).parse::<i32>().unwrap();
+        }
+        if cycles 
     }
     1
-
 }
 
 fn main() {
-    let input_text=read_data(); 
-    process_instructions(input_text); 
+    let input_text = read_data();
+    process_instructions(input_text);
 }
