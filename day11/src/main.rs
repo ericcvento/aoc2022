eb5227c6705f96395909f7466a4ab2bad67ef8e7
Eric Vento <vento.eric@gmail.com>
2023-01-08 10:52:54 -0500

    simplified count_monkeys

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -9,20 +9,26 @@ fn count_monkeys(input_string: &String) -> i32 {
     let mut monkey_n = 0;
     for line in input_string.lines() {
         let first_word = line.split_whitespace().nth(0);
-        match first_word {
-            Some(x) => {
-                if x == "Monkey" {
-                    monkey_n += 1;
-                }
-            }
-            None => {}
+        if first_word==Some("Monkey") {
+            monkey_n += 1;
         }
     }
     monkey_n
 }
 
+fn set_starting_inventories(input_string: &String) {
+    for line in input_string.lines() {
+        let mut instruction = line.trim().split(':').collect::<Vec<&str>>(); 
+        //instruction.retain(|&x| x != ""); 
+        if instruction[0]=="Starting items" {
+            println!("{:?}",instruction);
+        }
+    }
+
+}
+
 fn main() {
     let input_string = read_data();
     let monkey_n = count_monkeys(&input_string);
-    println!("{monkey_n}")
+    set_starting_inventories(&input_string);
 }
