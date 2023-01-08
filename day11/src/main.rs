081a3d1cea3edb9b9e1864466573f74d8a87681f
Eric Vento <vento.eric@gmail.com>
2023-01-08 05:58:30 -0500

    added function to count # of monkeys in puzzle input

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -5,6 +5,24 @@ fn read_data() -> String {
     ft
 }
 
+fn count_monkeys(input_string:&String)->i32 {
+    let mut monkey_n=0; 
+    for line in input_string.lines() {
+        let first_word = line.split_whitespace().nth(0); 
+        match first_word {
+            Some(x) => {
+                if x=="Monkey" {
+                    monkey_n +=1; 
+                }
+            }
+            None => {},
+        }
+    }
+    monkey_n
+}
+
 fn main() {
     let input_string=read_data(); 
+    let monkey_n = count_monkeys(&input_string); 
+    println!("{monkey_n}")
 }
