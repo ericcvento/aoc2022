30452b50509b394cc57702111138edc7f6acc686
Eric Vento <vento.eric@gmail.com>
2023-01-08 13:53:17 -0500

    started function to build Monkeys

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -3,10 +3,21 @@ use std::fs;
 struct Monkey {
     id: i32,
     inventory: Vec<i32>,
-    operation_str: String, 
-    test_divisor: i32, 
+    operation_str: String,
+    test_divisor: i32,
     true_monkey: i32,
-    false_monkey:i32
+    false_monkey: i32,
+}
+
+fn build_monkey(id: i32, inventory: Vec<i32>) -> Monkey {
+    Monkey {
+        id,
+        inventory,
+        operation_str: "".to_string(),
+        test_divisor: 0,
+        true_monkey: 99,
+        false_monkey: 99,
+    }
 }
 
 fn read_data() -> String {
@@ -18,7 +29,7 @@ fn count_monkeys(input_string: &str) -> i32 {
     let mut monkey_n = 0;
     for line in input_string.lines() {
         let first_word = line.split_whitespace().nth(0);
-        if first_word==Some("Monkey") {
+        if first_word == Some("Monkey") {
             monkey_n += 1;
         }
     }
@@ -26,13 +37,16 @@ fn count_monkeys(input_string: &str) -> i32 {
 }
 
 fn set_starting_inventories(input_string: &str) -> Vec<Vec<i32>> {
-    let mut monkeys_items=Vec::new(); 
+    let mut monkeys_items = Vec::new();
     for line in input_string.lines() {
-        let instruction = line.trim().split(':').collect::<Vec<&str>>(); 
-        if instruction[0]=="Starting items" {
-            let items=instruction[1].split(',').collect::<Vec<&str>>(); 
-            let items_n: Vec<i32>=items.iter().map(|x| x.trim().parse::<i32>().unwrap()).collect();
-            monkeys_items.push(items_n); 
+        let instruction = line.trim().split(':').collect::<Vec<&str>>();
+        if instruction[0] == "Starting items" {
+            let items = instruction[1].split(',').collect::<Vec<&str>>();
+            let items_n: Vec<i32> = items
+                .iter()
+                .map(|x| x.trim().parse::<i32>().unwrap())
+                .collect();
+            monkeys_items.push(items_n);
         }
     }
     monkeys_items
@@ -40,6 +54,11 @@ fn set_starting_inventories(input_string: &str) -> Vec<Vec<i32>> {
 
 fn main() {
     let input_string = read_data();
-    count_monkeys(&input_string);
-    let inventories=set_starting_inventories(&input_string);
+    let monkey_count = count_monkeys(&input_string);
+    let inventories = set_starting_inventories(&input_string);
+    let mut monkeys: Vec<Monkey> = Vec::new();
+    for i in 0..monkey_count {
+        monkeys.push(build_monkey(i, inventories[(i as usize)].clone()));
+    }
+    println!("{:?}", monkeys[0].inventory);
 }
