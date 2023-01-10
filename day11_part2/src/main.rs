47a22e517c3f8b69755ee5ddd1cc32f3b3aa5229
Eric Vento <vento.eric@gmail.com>
2023-01-09 23:32:01 -0500

    tried u128 for the inventories but it's not enough

--- day11_part2/src/main.rs
+++ day11_part2/src/main.rs
@@ -2,23 +2,20 @@ use std::fs;
 
 #[derive(Clone, Debug)]
 struct Monkey {
-    id: usize,
-    inventory: Vec<i32>,
+    inventory: Vec<u128>,
     operation_str: String,
-    test_divisor: i32,
+    test_divisor: u128,
     tfmonkeys: (usize, usize),
-    monkey_biz: i32,
+    monkey_biz: u128,
 }
 
 fn build_monkey(
-    id: usize,
-    inventory: Vec<i32>,
+    inventory: Vec<u128>,
     operation_str: String,
-    test_divisor: i32,
+    test_divisor: u128,
     tfmonkeys: (usize, usize),
 ) -> Monkey {
     Monkey {
-        id,
         inventory,
         operation_str,
         test_divisor,
@@ -27,15 +24,15 @@ fn build_monkey(
     }
 }
 
-fn set_starting_inventories(input_string: &str) -> Vec<Vec<i32>> {
+fn set_starting_inventories(input_string: &str) -> Vec<Vec<u128>> {
     let mut monkeys_items = Vec::new();
     for line in input_string.lines() {
         let instruction = line.trim().split(':').collect::<Vec<&str>>();
         if instruction[0] == "Starting items" {
             let items = instruction[1].split(',').collect::<Vec<&str>>();
-            let items_n: Vec<i32> = items
+            let items_n: Vec<u128> = items
                 .iter()
-                .map(|x| x.trim().parse::<i32>().unwrap())
+                .map(|x| x.trim().parse::<u128>().unwrap())
                 .collect();
             monkeys_items.push(items_n);
         }
@@ -54,7 +51,7 @@ fn parse_ops(input_string: &str) -> Vec<String> {
     ops
 }
 
-fn parse_divisors(input_string: &str) -> Vec<i32> {
+fn parse_divisors(input_string: &str) -> Vec<u128> {
     let mut divisors = Vec::new();
     for line in input_string.lines() {
         let instruction = line.trim().split(':').collect::<Vec<&str>>();
@@ -63,7 +60,7 @@ fn parse_divisors(input_string: &str) -> Vec<i32> {
                 .chars()
                 .filter(|c| c.is_ascii_digit())
                 .collect();
-            let divisor = t.parse::<i32>().unwrap();
+            let divisor = t.parse::<u128>().unwrap();
             divisors.push(divisor);
         }
     }
@@ -105,8 +102,8 @@ fn count_monkeys(input_string: &str) -> i32 {
     monkey_n
 }
 
-fn game_loop(mut monkeys: Vec<Monkey>) -> i32 {
-    for _round in 1..=20 {
+fn game_loop(mut monkeys: Vec<Monkey>) -> u128 {
+    for round in 1..=10000 {
         for m in 0..monkeys.len() {
             monkeys[m].inventory.reverse();
             //test worry, parsing this out in a ridiculous way
@@ -116,15 +113,17 @@ fn game_loop(mut monkeys: Vec<Monkey>) -> i32 {
             assert!(ops[3] == "*" || ops[3] == "+");
             for _i in 0..monkeys[m].inventory.len() {
                 let old = monkeys[m].inventory.pop().unwrap();
-                let comparator: i32 = if ops[4] == "old" {
+                let comparator: u128 = if ops[4] == "old" {
                     old
                 } else {
-                    ops[4].parse::<i32>().unwrap()
+                    ops[4].parse::<u128>().unwrap()
                 };
-                let new: i32 = if ops[3] == "*" {
-                    (old * comparator) / 3
+
+                println!("round: {round}, {}-{}",old,comparator);
+                let new: u128 = if ops[3] == "*" {
+                    old * comparator
                 } else {
-                    (old + comparator) / 3
+                    old + comparator
                 };
                 let relmonkey: usize = if (new % monkeys[m].test_divisor) == 0 {
                     monkeys[m].tfmonkeys.0
@@ -136,7 +135,7 @@ fn game_loop(mut monkeys: Vec<Monkey>) -> i32 {
             }
         }
     }
-    let mut monkey_biz_v: Vec<i32> = Vec::new();
+    let mut monkey_biz_v: Vec<u128> = Vec::new();
     for m in monkeys {
         monkey_biz_v.push(m.monkey_biz);
     }
@@ -162,7 +161,6 @@ fn main() {
     let mut monkeys: Vec<Monkey> = Vec::new();
     for i in 0..monkey_count {
         let monkey = build_monkey(
-            i,
             inventories[i].clone(),
             ops[i].clone(),
             divisors[i],
@@ -170,6 +168,9 @@ fn main() {
         );
         monkeys.push(monkey);
     }
-    let part1 = game_loop(monkeys);
-    println!("solution to part 1 is monkey Business: {part1}");
+    let part2 = game_loop(monkeys);
+    //println!("solution to part 1 is monkey Business: {part1}");
+
+    let bigone :u128 = 100_000_000_000; 
+    let bigone = bigone*bigone; 
 }
