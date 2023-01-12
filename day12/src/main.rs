185ce7e23a8ac0f908611abbd84ba6cb90ad424f
Eric Vento <vento.eric@gmail.com>
2023-01-12 10:05:07 -0500

    adding function to build out the grid

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,4 +1,25 @@
 use std::fs;
+use std::collections::HashMap; 
+
+struct Coordinates {
+    x:i32,
+    y:i32
+}
+fn load_Coordinates(x:i32,y:i32) -> Coordinates {
+    Coordinates {x,y}
+}
+
+fn build_grid(input_text:&str) -> HashMap<Coordinates,char> {
+    let grid = HashMap::new(); 
+    let mut y = 0;
+    for line in input_text.lines() {
+        let mut x = 0; 
+        for c in line.chars() {
+            
+        }
+    }
+    grid
+}
 
 fn read_data() -> String {
     let ft: String = fs::read_to_string(r"data\day12input.txt").expect("Invalid File.");
@@ -8,4 +29,5 @@ fn read_data() -> String {
 fn main() {
     let input_text=read_data(); 
     println!("{input_text}");
+    build_grid(&input_text); 
 }
