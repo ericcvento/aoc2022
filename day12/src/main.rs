c0c1120e1859184feba37c61265e28c1e621cbda
Eric Vento <vento.eric@gmail.com>
2023-01-12 11:21:06 -0500

    set up a function to build the starting grid

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,22 +1,18 @@
 use std::fs;
 use std::collections::HashMap; 
 
-struct Coordinates {
-    x:i32,
-    y:i32
-}
-fn load_Coordinates(x:i32,y:i32) -> Coordinates {
-    Coordinates {x,y}
-}
+type Coordinates = (i32, i32);
 
 fn build_grid(input_text:&str) -> HashMap<Coordinates,char> {
-    let grid = HashMap::new(); 
+    let mut grid = HashMap::new(); 
     let mut y = 0;
     for line in input_text.lines() {
         let mut x = 0; 
         for c in line.chars() {
-            
+            grid.insert((x,y),c);
+            x+=1; 
         }
+        y-=1; 
     }
     grid
 }
@@ -29,5 +25,10 @@ fn read_data() -> String {
 fn main() {
     let input_text=read_data(); 
     println!("{input_text}");
-    build_grid(&input_text); 
+    let the_grid=build_grid(&input_text);
+    for (k,v) in the_grid {
+        if v=='S' {
+            println!("{:?}",k);
+        } 
+    }
 }
