e7d07c523bfac239304573dea8c49acf262cbd63
Eric Vento <vento.eric@gmail.com>
2023-01-19 08:18:06 -0500

    experimenting with nom::delim

--- day13_part1/src/main.rs
+++ day13_part1/src/main.rs
@@ -1,17 +1,31 @@
 use std::fs;
 
+use nom::{
+    IResult,
+    sequence::delimited,
+    character::complete::char,
+    bytes::complete::is_not,
+    multi::separated_list0,
+    bytes::complete::tag
+  };
+  
+  fn parens(input: &str) -> IResult<&str, &str> {
+    delimited(char('['), is_not("]"), char(']'))(input)
+  }
+
 fn read_data() -> String {
     let ft: String = fs::read_to_string(r"data\day13input.txt").expect("Invalid File.");
     ft
 }
 
-fn convert_to_elements(input: &str) {
-    println!("{input}");
+fn parser(s: &str) -> IResult<&str, Vec<&str>> {
+    separated_list0(tag("],"),tag(""))(s)
 }
 
 fn main() {
     let input_text = read_data();
     for line in input_text.lines() {
-        convert_to_elements(line);
+        println!("{line}");
+        println!("{:?}",parens(&line));
     }
 }
